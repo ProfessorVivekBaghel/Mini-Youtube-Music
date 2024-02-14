41e92983f63e285a856d6e55b18bb19ba214fac8
Vivek Baghel <vivekbaghel1639@gmail.com>
2024-02-14 11:37:01 +0530

    Youtube music just for fun

---
+++ README.md
@@ -0,0 +1,21 @@
+# YouTube Music Web App
+
+Welcome to the Basic YouTube Music Web App! This web app allows you to search for and play music videos from YouTube right in your browser.
+
+## Features
+
+- Play music videos directly within the app.
+- Simple and intuitive user interface.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+
+
+## Demo
+
+(You can provide a link to a live demo of your web app here)
+
+
---
+++ script.js
@@ -0,0 +1,100 @@
+var arr = [
+    { songName: "Albele Tangewala", url: "./songs/Albela-Tangewala.mp3", img:"./images/albele.jpg"},
+    { songName: "Jale 2", url: "./songs/Jale 2.mp3", img: "./images/jale.jpg" },
+    { songName: "Pehle Bhi main", url: "./songs/Pehle Bhi Main.mp3", img: "./images/animal.jpg" },
+    { songName: "Ram siya ram", url: "./songs/Ram Siya Ram.mp3", img: "./images/ram.jpg" },
+    { songName: "Arjan Valley", url: "./songs/Arjan Vailly Ne.mp3", img: "./images/animal.jpg" }
+]
+var allSongs = document.querySelector("#all-songs")
+var poster = document.querySelector("#left")
+var video = document.querySelector("video");
+
+var main = document.querySelector("#main");
+
+var play = document.querySelector("#play")
+var backward = document.querySelector("#backward")
+var forward = document.querySelector("#forward")
+
+
+
+var audio = new Audio()
+
+var selectedSong = 0
+
+function mainFunction() {
+    var clutter = ""
+
+    arr.forEach(function (elem, index) {
+        clutter += `<div class="song-card" id=${index}>
+    <div class="part1">
+        <img src=${elem.img} alt="">
+        <h2>${elem.songName}</h2>
+    </div>
+    <h6>3:56</h6>
+</div>`
+    })
+    allSongs.innerHTML = clutter
+
+    audio.src = arr[selectedSong].url
+    poster.style.backgroundImage = `url(${arr[selectedSong].img})`
+}
+mainFunction()
+
+allSongs.addEventListener("click", function (dets) {
+    selectedSong = dets.target.id
+    mainFunction()
+    play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+    video.style.display = "block";
+    main.style.backgroundColor = "transparent";
+    flag = 1
+    audio.play()
+})
+
+var flag = 0
+
+play.addEventListener("click", function () {
+    if (flag == 0) {
+        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        mainFunction()
+        audio.play()
+        video.style.display = "block";
+        main.style.backgroundColor = "transparent";
+        flag = 1
+        
+    } else {
+        play.innerHTML = `<i class="ri-play-mini-fill"></i>`
+        mainFunction()
+        audio.pause()
+        flag = 0
+        video.style.display = "none";
+        main.style.backgroundColor = "#111";
+    }
+})
+
+forward.addEventListener("click", function () {
+    if (selectedSong < arr.length - 1) {
+        selectedSong++
+        mainFunction()
+        audio.play()
+        video.style.display = "block";
+        main.style.backgroundColor = "transparent";
+    }else{
+        forward.style.opacity = 0.4
+        video.style.display = "none";
+        main.style.backgroundColor = "#111";
+        
+    }
+})
+backward.addEventListener("click", () => {
+        if (selectedSong > 0) {
+            selectedSong--;
+            mainFunction();
+            audio.play();
+            video.style.display = "none";
+            main.style.backgroundColor = "transparent";
+        } else {
+            backward.style.opacity = 0.4;
+            video.style.display = "none";
+            main.style.backgroundColor = "#111";
+        }
+    })
